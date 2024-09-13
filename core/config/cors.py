67586ca4ae4233db56205910a21db00f8dd8a203
ahmedhamila.stage <77456894+ahmedhamila@users.noreply.github.com>
2024-09-13 20:57:03 +0100

    Update cors.py

--- core/config/cors.py
+++ core/config/cors.py
@@ -1,2 +1,3 @@
 CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ["django-project-template.azurewebsites.net"]
+CSRF_TRUSTED_ORIGINS = ["https://django-project-template.azurewebsites.net", "127.0.0.1"]
