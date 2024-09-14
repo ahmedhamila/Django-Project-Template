0c5b0c8b1e81bf3515c943a96ae87584b1efa446
ahmedhamila.stage <77456894+ahmedhamila@users.noreply.github.com>
2024-09-14 18:27:38 +0100

    testing csrf trusted foor drf

--- core/config/cors.py
+++ core/config/cors.py
@@ -1,3 +1,3 @@
 CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ["django-project-template.azurewebsites.net", "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["https://django-project-template.azurewebsites.net", "http://127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://django-project-template.azurewebsites.net", "http://127.0.0.1","http://django-project-template.azurewebsites.net"]
